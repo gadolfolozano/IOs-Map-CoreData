e0f0d5382939e66cfe9a86f57b1a3febf26eb415
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2018-02-17 20:44:43 -0500

    first commit

---
+++ MapsAndCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapsAndCoreData
+//
+//  Created by Adolfo Lozano Mendez on 17/02/18.
+//  Copyright © 2018 Adolfo Lozano Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
