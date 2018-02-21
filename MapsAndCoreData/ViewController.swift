783b64e7671e91f59994c5514660edd716660c15
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2018-02-20 23:59:33 -0500

    add localization permission to plist, and show actual location in map

--- MapsAndCoreData/ViewController.swift
+++ MapsAndCoreData/ViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mMapView: MKMapView!
+    var mLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mMapView.delegate = self
+        
+        mLocationManager.delegate = self
+        mLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        mLocationManager.requestWhenInUseAuthorization()
+        mLocationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        self.mMapView.setRegion(region, animated: true)
+        
     }
 
 
